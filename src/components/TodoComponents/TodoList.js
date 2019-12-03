89cf95ac7ee53c7caa09d50fc6c7921a8eb8e1cc
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-03 22:03:50 +0100

    finished the feature of the todo app

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
+import React from "react";
+import Todo from "./Todo";
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = this.props.atask;
+
+    return (
+      <div>
+        {data.map(task => (
+          <Todo taskName={task.task} />
+        ))}
+      </div>
+    );
+  }
+}
+export default TodoList;
